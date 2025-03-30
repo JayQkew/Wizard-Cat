bc5e344c67bb231b385cf8f4bef5fdb6d9f64c58
JayQkew <rayjebb@gmail.com>
2025-03-30 23:38:26 +0200

    Starting InputHandler

--- Wizard Cat/Assets/_Scripts/InputHandler.cs
+++ Wizard Cat/Assets/_Scripts/InputHandler.cs
@@ -1,16 +1,32 @@
+using System;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
 public class InputHandler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public static InputHandler Instance { get; private set; }
+    private PlayerInput playerInput;
 
-    // Update is called once per frame
-    void Update()
+    public Vector2 moveInput;
+    public Vector2 aimInput;
+
+    [HideInInspector] public UnityEvent onShoot;
+    [HideInInspector] public UnityEvent onShootStart;
+    [HideInInspector] public UnityEvent onShootEnd;
+    [HideInInspector] public UnityEvent onMelee;
+    [HideInInspector] public UnityEvent onMeleeStart;
+    [HideInInspector] public UnityEvent onMeleeEnd;
+    [HideInInspector] public UnityEvent onDash;
+    [HideInInspector] public UnityEvent onDashStart;
+    [HideInInspector] public UnityEvent onDashEnd;
+    [HideInInspector] public UnityEvent onInteract;
+    
+    private void Awake()
     {
+        if (Instance == null) Instance = this;
+        else Destroy(this);
         
+        playerInput = GetComponent<PlayerInput>();
     }
 }
